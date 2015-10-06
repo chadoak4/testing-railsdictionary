d3021df8e0a65b3ebda3eb23c7a5ee9985740d77
Chad Oakley <Chad.Oakley@tampabay.rr.com>
2015-10-06 09:05:30 -0400

    explorer mode

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -1,4 +1,8 @@
 class Definition < ActiveRecord::Base
   validates :word, presence: true
   validates :meaning, presence: true
+
+  def fancy?
+    self.word.length > 16
+  end
 end
--- test/models/definition_test.rb
+++ test/models/definition_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class DefinitionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+
+  test "definitions without a word/meaning are invalid" do
+     definition = Definition.new
+
+     refute definition.valid?
+   end
+
+  test "books must have a title/meaning to be valid" do
+    definition = Definition.new(word: "meow",meaning:"cat talk")
+
+    assert definition.valid?, "Definitions without a word should not be valid"
+  end
+
+
+  test "Words whose length is 16 characters or more should be considered fancy" do
+      definition = Definition.new(word: "mynameischadoakleylololol")
+
+      assert definition.fancy?, "A 16 character word should be a fancy word"
+  end
+
 end
