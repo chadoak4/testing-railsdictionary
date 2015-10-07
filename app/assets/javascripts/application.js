fefe9ba74db7dd0e7f1d296540b5a33f5e25b40c
Chad Oakley <Chad.Oakley@tampabay.rr.com>
2015-10-07 02:18:23 -0400

    logo update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,9 +25,3 @@ $(document).ready(function () {
      $(".pr-wrap").removeClass("show-pass-reset");
    });
 });
-
-$(function() {
-    var BV = new $.BigVideo();
-    BV.init();
-    BV.show('http://vjs.zencdn.net/v/oceans.mp4',{ambient:true});
-});
